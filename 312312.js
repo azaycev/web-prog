3a5fc367636231e8383dde8ce8ee04193ca05669
azaycev <Avatarza1997@gmail.com>
2016-09-18 20:56:14 +0300

    makeArmy

---
+++ 312312.js
@@ -0,0 +1,19 @@
+function makeArmy() {
+
+      var shooters = [];
+
+      for (var i = 0; i < 10; i++) {
+        var shooter = function me(i) { 
+          alert(me.i); 
+        };
+        shooter.i=i;
+        shooters.push(shooter);
+      };
+
+      return shooters;
+    };
+
+    var army = makeArmy();
+
+    army[0]();
+    army[5](); 
